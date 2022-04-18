8c0907535b79ad1ba24c219c29059188872bb802
Selman Kirman <selmankirman@gmail.com>
2022-04-18 14:16:40 +0200

    selam git

---
+++ src/day07_ifElseStatements/C01_IfStatements.java
@@ -0,0 +1,15 @@
+package day07_ifElseStatements;
+
+public class C01_IfStatements {
+
+    public static void main(String[] args) {
+
+
+        int a = 2;
+        int b = 3;
+
+        if(a==b){
+
+        }
+    }
+}
